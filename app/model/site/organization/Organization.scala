17bbbcd3d95226b75a66ceb15cedb47731c15b1c
Tirol_JPN <Tirol.JPN@gmail.com>
2019-07-27 17:21:55 +0900

    add siteViewValue of organization

---
+++ app/model/site/organization/Organization.scala
@@ -0,0 +1,13 @@
+package model.site.organization
+
+import model.component.util.ViewValuePageLayout
+import persistence.geo.model.Location
+import persistence.organization.model.Organization
+
+// 単体の組織
+class Organization
+(
+  layout:      ViewValuePageLayout,
+  location:    Seq[Location],
+  organzation: Option[Organization]
+)
